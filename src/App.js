478c02f8e8a8d2bce640ba4f44dd33836a08ecf1
unknown <yury.ovchinnkov@yandex.ru>
2023-05-24 15:01:35 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [timer, setTimer] = useState(0);
+  const [date, setDate] = useState(0);
+  const [dateNow, setDateNow] = useState(0);
+  const [time, setTime] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minuts, setMinuts] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  function ChangeTimer(e) {
+    setTimer(e.target.value);
+  }
+
+  function StartTimer() {
+    setDate(new Date(Date.now() + timer * 1000));
+    setDateNow(Date.now());
+    setTime(~~((date - dateNow) / 1000));
+    setHours(Math.floor(time / (60 * 60)));
+    setMinuts(Math.floor(time / 60));
+    setSeconds(Math.floor(time % 60));
+    setTime(~~((date - dateNow) / 1000));
+  }
+
+  useEffect(() => {
+    if (time > 0) {
+      const interval = setInterval(() => {
+        setHours(Math.floor((time / (60 * 60)) % 24));
+        setMinuts(Math.floor((time / 60) % 60));
+        setSeconds(Math.floor(time % 60));
+        setDateNow(Date.now());
+        clearInterval(interval);
+        setTime(~~((date - dateNow) / 1000));
+      }, 100);
+    } else {
+      setHours(0);
+      setMinuts(0);
+      setSeconds(0);
+    }
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input placeholder="Seconds" type="number" onChange={ChangeTimer} />
+
+      <button onClick={StartTimer}>Start</button>
+
+      <br />
+      <br />
+
+      <span>
+        {hours} : {minuts} : {seconds}
+      </span>
+    </>
   );
 }
 
